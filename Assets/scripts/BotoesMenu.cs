2e03ce329277ce454392e4eeacc303a11f81ebc9
ROSBEILYS VALENTINA TIZAMO CHENG <rosbeilys_cheng@estudante.sesisenai.org.br>
2025-11-27 10:32:38 -0300

    commit inicial com gitignore

---
+++ Assets/scripts/BotoesMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BotoesMenu : MonoBehaviour
+{
+
+
+   public void IniciarJogo()
+   {
+      SceneManager.LoadScene("Jogo");
+   }
+
+   public void SairDoJogo()
+   {
+      Application.Quit();
+   }
+}
+
+
+
+
